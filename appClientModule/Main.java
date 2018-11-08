2554118fa14df807b49102d2b5f0b657b2daa402
chuzunfei <chuzunfei@users.noreply.github.com>
2018-11-08 22:56:39 +0800

    main commit

--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -20,6 +20,7 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 import javax.media.j3d.Alpha;
@@ -29,13 +30,13 @@ import javax.media.j3d.Background;
 import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Canvas3D;
-import javax.media.j3d.ColoringAttributes;
 import javax.media.j3d.DirectionalLight;
 import javax.media.j3d.Font3D;
 import javax.media.j3d.FontExtrusion;
 import javax.media.j3d.GraphicsConfigTemplate3D;
 import javax.media.j3d.Group;
 import javax.media.j3d.LineAttributes;
+import javax.media.j3d.LineStripArray;
 import javax.media.j3d.Material;
 import javax.media.j3d.RotationInterpolator;
 import javax.media.j3d.Shape3D;
@@ -64,9 +65,11 @@ import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.vecmath.AxisAngle4d;
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3d;
 import javax.vecmath.Point3f;
+import javax.vecmath.Quat4f;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
 
@@ -74,8 +77,11 @@ import org.fem.object.Line;
 import org.fem.object.Point;
 
 import com.sun.j3d.exp.swing.JCanvas3D;
+import com.sun.j3d.utils.behaviors.mouse.MouseBehavior;
 import com.sun.j3d.utils.behaviors.mouse.MouseRotate;
+import com.sun.j3d.utils.behaviors.mouse.MouseWheelZoom;
 import com.sun.j3d.utils.behaviors.vp.OrbitBehavior;
+import com.sun.j3d.utils.geometry.Cylinder;
 import com.sun.j3d.utils.universe.SimpleUniverse;
 import com.sun.j3d.utils.universe.ViewingPlatform;
 
@@ -92,8 +98,13 @@ public class Main extends JFrame {
 	JLabel labelX = new JLabel("0");
 	JLabel labelY = new JLabel("0");
 	JLabel labelZ = new JLabel("0");
+	Color3f red = new Color3f(1.0f, 0.0f, 0.0f);
+	Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
+	Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
 	static GraphicsConfiguration gc = SimpleUniverse.getPreferredConfiguration();
+	DrawPanel jp = new DrawPanel();
 	SimpleUniverse u = null;
+	SimpleUniverse universe = null;
 
 	/**
 	 * Launch the application.
@@ -120,7 +131,6 @@ public class Main extends JFrame {
 		setLayout(new BorderLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1200, 900);
-
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
 
@@ -248,6 +258,11 @@ public class Main extends JFrame {
 					}
 
 					public void windowClosed(WindowEvent e) {
+						objRoot.detach();
+						objTrans.addChild(generateCylinder(line.getEndpoints()));
+//						objRoot.addChild(objTrans);
+				      
+						universe.addBranchGraph(objRoot);
 						JPanel panel = new JPanel();
 
 						// setClosable(true);
@@ -266,18 +281,129 @@ public class Main extends JFrame {
 //
 //		// setClosable(true);
 //
-		panel.setBackground(Color.BLUE);
-		draw3d(panel, line);
+		
+		JCanvas3D canvas = new JCanvas3D(new GraphicsConfigTemplate3D());
+		canvas.setResizeMode(JCanvas3D.RESIZE_IMMEDIATELY);
+//		setLayout(new BorderLayout());
+//		GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+//		Canvas3D canvas = new Canvas3D(config);
+//		add("Center", canvas);
+		Dimension dim = new Dimension(256, 256);
+		canvas.setPreferredSize(dim);
+		canvas.setSize(dim);
+		panel.setLayout(new BorderLayout());
+		panel.add(canvas, BorderLayout.CENTER);
+		
+		BranchGroup objRoot = createSceneGraph(canvas);
 
+		universe = new SimpleUniverse(canvas.getOffscreenCanvas3D()); // TODO: this is awful and must not
+																						// be done like that in final
+																						// version
+		universe.addBranchGraph(objRoot);
+
+		// This will move the ViewPlatform back a bit so the
+		// objects in the scene can be viewed.
+		ViewingPlatform viewingPlatform = universe.getViewingPlatform();
+		viewingPlatform.setNominalViewingTransform();
+		OrbitBehavior orbit = new OrbitBehavior(canvas.getOffscreenCanvas3D(),
+				OrbitBehavior.REVERSE_ALL | OrbitBehavior.STOP_ZOOM);
+		BoundingSphere bounds2 = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
+		orbit.setSchedulingBounds(bounds2);
+		viewingPlatform.setViewPlatformBehavior(orbit);
+		
 		contentPane.add(panel, BorderLayout.CENTER);
-		System.out.printf("contentPane,height:%f, width:%d \n",contentPane.getMaximumSize().getHeight(),contentPane.getWidth());
-	}
 
-	private void draw3d(JPanel panel, Line line) {
+	}
+	
+	TransformGroup objTrans = null;
+	BranchGroup objRoot = null;
+	BranchGroup createSceneGraph(Component comp) {
+		
+		objRoot = new BranchGroup();
+		objRoot.setCapability(BranchGroup.ALLOW_DETACH);
+		objRoot.setCapability(BranchGroup.ALLOW_CHILDREN_EXTEND);
+		objRoot.setCapability(BranchGroup.ALLOW_CHILDREN_READ);
+		objRoot.setCapability(BranchGroup.ALLOW_CHILDREN_WRITE);
+		TransformGroup objScale = new TransformGroup();
+		Transform3D scaleTrans = new Transform3D();
+		scaleTrans.set(1 / 16.5f);
+		objScale.setTransform(scaleTrans);
+		objRoot.addChild(objScale);
+		objTrans = new TransformGroup();
+		objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+		objScale.addChild(objTrans);
+		objTrans.addChild(draw3d());
+		BoundingSphere bounds = new BoundingSphere(new Point3d(), 100.0);
+		Background bg = new Background(new Color3f(1.0f, 1.0f, 1.0f));
+		bg.setApplicationBounds(bounds);
+		objTrans.addChild(bg);
+		MouseRotate mr = new MouseRotate(comp);
+		mr.setTransformGroup(objTrans);
+		mr.setSchedulingBounds(bounds);
+		mr.setFactor(0.007);
+		objTrans.addChild(mr);
+		Color3f ambientColor = new Color3f(0.1f, 0.1f, 0.1f);
+		AmbientLight ambientLightNode = new AmbientLight(ambientColor);
+		ambientLightNode.setInfluencingBounds(bounds);
+		objRoot.addChild(ambientLightNode);
+		Color3f light1Color = new Color3f(1.0f, 1.0f, 1.0f);
+		Vector3f light1Direction = new Vector3f(0.0f, -0.2f, -1.0f);
+		DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
+		light1.setInfluencingBounds(bounds);
+		objRoot.addChild(light1);
+		return objRoot;
+	}
+	
+	private TransformGroup addAixs() {
+		TransformGroup tg = new TransformGroup();
+		int StripCount[] = new int[1];  
+		  
+	    float vert[] = {   
+	    		 0.0f,0.0f,0.0f,  
+	 	        15f,0.0f,0.0f,  
+	 	        0.0f,0.0f,0.0f,  
+	 	        0.0f,15.0f,0.0f,  
+	 	        0.0f,0.0f,0.0f,  
+	 	        0.0f,0.0f,15.0f,  
+	       };  
+	  
+	    float color[] = {  
+	    		 0.0f,0.5f,1.0f,  
+	    	        0.5f,0.0f,1.0f,  
+	    	        0.0f,0.8f,2.0f,  
+	    	        1.0f,0.0f,0.3f,  
+	    	        0.0f,1.0f,0.3f,  
+	    	        0.3f,0.8f,0.0f,  
+	      };  
+		StripCount[0] = 6;  
+		  
+        LineStripArray line = new LineStripArray(6,   
+                  LineStripArray.COORDINATES|  
+                  LineStripArray.COLOR_3,StripCount);  
+          line.setCoordinates(0,vert);  
+          line.setColors(0,color);  
+        LineAttributes la = new LineAttributes();  
+          la.setLineWidth(3.0f);  
+          la.setLineAntialiasingEnable(true);  
+          la.setLinePattern(LineAttributes.PATTERN_SOLID);  
+        Appearance ap = new Appearance();  
+         ap.setLineAttributes(la);
+         
+         Shape3D shape = new Shape3D();
+  		shape.setGeometry(line);
+  		shape.setAppearance(ap);
+  		
+  		tg.addChild(shape);
+  		return tg;
+	}
+	private TransformGroup draw3d() {
+		Line line = new Line();
+		line.setEndpoints(new ArrayList<>());
 		Point point = new Point();
 		point.setX(0);
 		point.setY(0);
-		point.setZ(0);
+		point.setZ((float) 0.5);
 		line.getEndpoints().add(point);
 		Point point1 = new Point();
 		point1.setX(0);
@@ -289,39 +415,104 @@ public class Main extends JFrame {
 		point2.setY(3);
 		point2.setZ(1);
 		line.getEndpoints().add(point2);
-//		Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
-		JCanvas3D canvas = new JCanvas3D(new GraphicsConfigTemplate3D());
-		canvas.setResizeMode(JCanvas3D.RESIZE_IMMEDIATELY);
-		Component comp = canvas;
-		System.out.printf("canvas,height:%f, width:%d \n",canvas.getMaximumSize().getHeight(),canvas.getWidth());
-		Dimension dim = new Dimension(256, 256);
-		comp.setPreferredSize(dim);
-		comp.setSize(dim);
-		// getContentPane().setLayout(new BorderLayout());
-		// getContentPane().add(comp, BorderLayout.CENTER);
-		// pack();
-		panel.setLayout(new BorderLayout());
-		panel.add(comp, BorderLayout.CENTER);
-		System.out.printf("panel,height:%f, width:%d \n",panel.getMaximumSize().getHeight(),panel.getWidth());
-		// Create a simple scene and attach it to the virtual universe
-		// BranchGroup scene = createSceneGraph(isInteractive, isRandom);
-		BranchGroup objRoot = new BranchGroup();
 
-		// Create the TransformGroup node and initialize it to the
-		// identity. Enable the TRANSFORM_WRITE capability so that
-		// our behavior code can modify it at run time. Add it to
-		// the root of the subgraph.
+		return generateCylinder(line.getEndpoints());
+	}
+
+	public TransformGroup generateCylinder(List<Point> pList) {
+		TransformGroup tg = new TransformGroup();
+		tg.addChild(addAixs());
+		Material material = new Material(red, black, red, white, 64);
+		Appearance appearance = new Appearance();
+		appearance.setMaterial(material);
+		Vector3f tmpVector = new Vector3f();
+		Transform3D tmpTrans = new Transform3D();
+		TransformGroup tmpTG = null;
+		for (int i=1;i<pList.size();i++) {
+			Point p1 = pList.get(i-1);
+			Point p2 = pList.get(i);
+			double len = Point.distance(p1, p2);
+			
+			TransformGroup cg = new TransformGroup();
+			cg.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+			cg.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+			
+			tmpVector.set((float) p1.getX(), (float) p1.getY(), (float) p1.getZ());
+			tmpTrans.set(tmpVector);
+			cg.setTransform(tmpTrans);
+
+			tmpTG = new TransformGroup();
+			tmpVector.set(0, (float) len/2, 0);
+			tmpTrans.set(tmpVector);
+//			tmpTrans.setScale(0.1);
+			tmpTG.setTransform(tmpTrans);
+			Cylinder tmpCyl = new Cylinder(0.1f, (float) len, appearance);
+			tmpTG.addChild(tmpCyl);
+			cg.addChild(tmpTG);
+
+			cg.getTransform(tmpTrans);
+			tmpTrans.setRotation(makeAA(p1, p2, len));
+			cg.setTransform(tmpTrans);
+
+			tg.addChild(cg);
+		}
+
+		return tg;
+	}
+	
+	/*
+	 *  x = sin(Y/2)sin(Z/2)cos(X/2)+cos(Y/2)cos(Z/2)sin(X/2)
+		y = sin(Y/2)cos(Z/2)cos(X/2)+cos(Y/2)sin(Z/2)sin(X/2)
+		z = cos(Y/2)sin(Z/2)cos(X/2)-sin(Y/2)cos(Z/2)sin(X/2)
+		w = cos(Y/2)cos(Z/2)cos(X/2)-sin(Y/2)sin(Z/2)sin(X/2)
+		q = ((x, y, z), w)
+	 */
+	private Quat4f makeQuat4f(double anglex,double angley,double anglez) {
+		float x = (float) (Math.sin(angley/2)*Math.sin(anglez/2)*Math.cos(anglex/2) + Math.cos(angley/2)*Math.cos(anglez/2)*Math.sin(anglex/2));
+		float y = (float) (Math.sin(angley/2)*Math.cos(anglez/2)*Math.cos(anglex/2) + Math.cos(angley/2)*Math.sin(anglez/2)*Math.sin(anglex/2));
+		float z = (float) (Math.cos(angley/2)*Math.sin(anglez/2)*Math.cos(anglex/2) - Math.sin(angley/2)*Math.cos(anglez/2)*Math.sin(anglex/2));
+		float w = (float) (Math.cos(angley/2)*Math.cos(anglez/2)*Math.cos(anglex/2) - Math.sin(angley/2)*Math.sin(anglez/2)*Math.sin(anglex/2));
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(w);
+		return new Quat4f(x,y,z,w);
+	}
+	
+	private AxisAngle4d makeAA(Point s, Point d, double len) {
+		AxisAngle4d aa = new AxisAngle4d();
+		if (len == 0) {
+			len = Math.sqrt(Math.pow(d.getX()-s.getX(), 2) + Math.pow(d.getY()-s.getY(), 2) + Math.pow(d.getZ()-s.getZ(), 2));
+		}
+		Vector3d vs = new Vector3d(0, len, 0);
+		Vector3d vd = new Vector3d(d.getX()-s.getX(), d.getY()-s.getY(), d.getZ()-s.getZ());
+		
+		Vector3d vr = new Vector3d();
+		vr.cross(vs, vd);
+		double angle = Math.acos(vs.dot(vd)/(vs.length()*vd.length()));
+		
+		aa.set(vr, angle);
+		
+		return aa;
+	}
+	
+	BranchGroup createSceneGraphOld(JCanvas3D canvas) {
+		BranchGroup objRoot = new BranchGroup();
+		
+		
 		TransformGroup objTrans = new TransformGroup();
+//		objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+//		objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 		Transform3D t3dTrans = new Transform3D();
 		t3dTrans.setTranslation(new Vector3d(0, 0, -1));
 		objTrans.setTransform(t3dTrans);
-		
+
 		TransformGroup objRot = new TransformGroup();
+		objRot.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
 		objRot.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
 		objTrans.addChild(objRot);
 		objRoot.addChild(objTrans);
-
-
+		
 		Shape3D sh = new Shape3D();
 		Appearance app = new Appearance();
 		Material mm = new Material();
@@ -337,136 +528,73 @@ public class Main extends JFrame {
 		Appearance appearance = new Appearance();
 		// appearance.setLineAttributes(la);
 		appearance.setMaterial(mm);
-		Shape3D shape = new Shape3D();
-		shape.setGeometry(line.generateLineShape());
-		shape.setAppearance(appearance);
-		objRot.addChild(shape);
+		
+		
+		TransformGroup objRot2 = new TransformGroup();
+		objRot.addChild(draw3d(new Line()));
 
 		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
 
 		// Set up the ambient light
-//		Color3f ambientColor = new Color3f(0.3f, 0.3f, 0.3f);
-//		AmbientLight ambientLightNode = new AmbientLight(ambientColor);
-//		ambientLightNode.setInfluencingBounds(bounds);
-//		objRoot.addChild(ambientLightNode);
+		Color3f ambientColor = new Color3f(0.3f, 0.3f, 0.3f);
+		AmbientLight ambientLightNode = new AmbientLight(ambientColor);
+		ambientLightNode.setInfluencingBounds(bounds);
+		objRoot.addChild(ambientLightNode);
 
 		// Set up the directional lights
-//		Color3f light1Color = new Color3f(1.0f, 1.0f, 0.9f);
-//		Vector3f light1Direction = new Vector3f(1.0f, 1.0f, 1.0f);
-//		Color3f light2Color = new Color3f(1.0f, 1.0f, 0.9f);
-//		Vector3f light2Direction = new Vector3f(-1.0f, -1.0f, -1.0f);
-//
-//		DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
-//		light1.setInfluencingBounds(bounds);
-//		objRoot.addChild(light1);
-//
-//		DirectionalLight light2 = new DirectionalLight(light2Color, light2Direction);
-//		light2.setInfluencingBounds(bounds);
-//		objRoot.addChild(light2);
+		Color3f light1Color = new Color3f(1.0f, 1.0f, 0.9f);
+		Vector3f light1Direction = new Vector3f(1.0f, 1.0f, 1.0f);
+		Color3f light2Color = new Color3f(1.0f, 1.0f, 0.9f);
+		Vector3f light2Direction = new Vector3f(-1.0f, -1.0f, -1.0f);
+
+		DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
+		light1.setInfluencingBounds(bounds);
+		objRoot.addChild(light1);
+
+		DirectionalLight light2 = new DirectionalLight(light2Color, light2Direction);
+		light2.setInfluencingBounds(bounds);
+		objRoot.addChild(light2);
 
 		boolean isInteractive = true;
 		if (true == isInteractive) {
-			MouseRotate mr = new MouseRotate(comp, objRot);
+			MouseRotate mr = new MouseRotate(canvas.getOffscreenCanvas3D(),objRot);
+//			mr.setTransformGroup(objRot);
 			mr.setSchedulingBounds(bounds);
 			mr.setSchedulingInterval(1);
 			objRoot.addChild(mr);
-		} else {
-			// Create a new Behavior object that will perform the
-			// desired operation on the specified transform and add
-			// it into the scene graph.
-			Transform3D yAxis = new Transform3D();
-
-			// rotation speed is randomized a bit so that it does not go at the same speed
-			// on every canvases,
-			// which will make it more natural and express the differences between every
-			// present universes
-			Alpha rotationAlpha = null;
-
-			boolean isRandom = true;
-			if (true == isRandom) {
-				int duration = Math.max(2000, (int) (Math.random() * 8000.));
-				rotationAlpha = new Alpha(-1, (int) ((double) duration * Math.random()), 0, duration, 0, 0);
-			} else {
-				rotationAlpha = new Alpha(-1, 4000);
-			}
-			RotationInterpolator rotator = new RotationInterpolator(rotationAlpha, objRot, yAxis, 0.0f,
-					(float) Math.PI * 2.0f);
-
-			rotator.setSchedulingBounds(bounds);
-			objRoot.addChild(rotator);
 		}
+		objRoot.compile();
 		
-		Background bg=new Background(new Color3f(Color.white));
-		bg.setApplicationBounds(bounds);
-		objRoot.addChild(bg);
-		BranchGroup scene = objRoot;
-
-		SimpleUniverse universe = new SimpleUniverse(canvas.getOffscreenCanvas3D()); // TODO: this is awful and must not
-																						// be done like that in final
-																						// version
-		universe.addBranchGraph(scene);
-
-		// This will move the ViewPlatform back a bit so the
-		// objects in the scene can be viewed.
-		ViewingPlatform viewingPlatform = universe.getViewingPlatform();
-		viewingPlatform.setNominalViewingTransform();
-		OrbitBehavior orbit = new OrbitBehavior(canvas.getOffscreenCanvas3D(),
-				OrbitBehavior.REVERSE_ALL | OrbitBehavior.STOP_ZOOM);
-		BoundingSphere bounds2 = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 100.0);
-		orbit.setSchedulingBounds(bounds2);
-		viewingPlatform.setViewPlatformBehavior(orbit);
-		// universe.getViewer().getView().setMinimumFrameCycleTime(30);
-
-	}
-
-	private BranchGroup createSceneGraph(Line line) {
-		// BranchGroup objRoot = new BranchGroup();
-		// objRoot.addChild(createObject(line));
-		// objRoot.compile();
-		// return objRoot;
-
-		BranchGroup objRoot = new BranchGroup();
-
-		// Create the TransformGroup node and initialize it to the
-		// identity. Enable the TRANSFORM_WRITE capability so that
-		// our behavior code can modify it at run time. Add it to
-		// the root of the subgraph.
-		TransformGroup objTrans = new TransformGroup();
-		Transform3D t3dTrans = new Transform3D();
-		t3dTrans.setTranslation(new Vector3d(0, 0, -1));
-		objTrans.setTransform(t3dTrans);
-
-		TransformGroup objRot = new TransformGroup();
-		objRot.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-		objTrans.addChild(objRot);
-		objRoot.addChild(objTrans);
 		return objRoot;
 	}
+	
+	private TransformGroup draw3d(Line line) {
+		line.setEndpoints(new ArrayList<>());
+		Point point = new Point();
+		point.setX(0);
+		point.setY((float) 0.5);
+		point.setZ(0);
+		line.getEndpoints().add(point);
+		Point point1 = new Point();
+		point1.setX(1);
+		point1.setY(1);
+		point1.setZ(1);
+		line.getEndpoints().add(point1);
+		Point point2 = new Point();
+		point2.setX(6);
+		point2.setY(6);
+		point2.setZ(0);
+		line.getEndpoints().add(point2);
+//		Canvas3D canvas = new Canvas3D(SimpleUniverse.getPreferredConfiguration());
+		
+		
+		
+		return line.generateCylinder();
 
-	private Group createObject(Line line) {
-		Transform3D t = new Transform3D();
-		TransformGroup objTrans = new TransformGroup(t);
-		objTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+		
 
-		LineAttributes la = new LineAttributes();
-		la.setLineWidth(30.0f);
-		la.setLineAntialiasingEnable(true);
-		la.setLinePattern(LineAttributes.PATTERN_DASH);
-		Appearance appearance = new Appearance();
-		appearance.setLineAttributes(la);
-		Shape3D shape = new Shape3D(line.generateLineShape(), appearance);
-		objTrans.addChild(shape);
-
-		Transform3D yAxis = new Transform3D();
-		Alpha rotationAlpha = new Alpha(-1, Alpha.INCREASING_ENABLE, 0, 0, 4000, 0, 0, 0, 0, 0);
-		RotationInterpolator rotator = new RotationInterpolator(rotationAlpha, objTrans, yAxis, 0.0f,
-				(float) Math.PI * 2.0f);
-		BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 50.0);
-		rotator.setSchedulingBounds(bounds);
-		objTrans.addChild(rotator);
-
-		return objTrans;
 	}
+
 
 	class DrawPanel extends Canvas3D implements MouseListener, MouseMotionListener {
 
